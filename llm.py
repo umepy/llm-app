da8551b81839ae070ce7b3cf88dad56f0cee6958
umepy <haruya04183083@gmail.com>
2023-12-21 17:33:13 +0900

    fix api_key (can't use now)

--- llm.py
+++ llm.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 
-client = OpenAI(api_key="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")
+client = OpenAI(api_key="ここにAPIキーを入力")
 model = "gpt-3.5-turbo"
 
 def llm_response(chat_history):
