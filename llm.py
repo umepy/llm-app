5c89abe3e21fea7ee64c7b4947fe83811c2c1453
umepy <haruya04183083@gmail.com>
2023-12-26 14:10:15 +0900

    comment out using llm

--- llm.py
+++ llm.py
@@ -1,19 +1,18 @@
 from openai import OpenAI
 
-from local_llm import get_local_llm_response
-from prompt import get_local_llm_prompt
+#from local_llm import get_local_llm_response
+#from prompt import get_local_llm_prompt
 
 client = OpenAI(api_key="ここにAPIキーを入力")
-model = "gpt-3.5-turbo"
 
 def llm_response(model_name, chat_history):
     model_name = model_name_convert(model_name)
-    if model_name == "gpt-3.5-turbo":
-        response = client.chat.completions.create(model=model, messages=chat_history)
+    if model_name == "gpt-3.5-turbo" or model_name == "gpt-4-1106-preview":
+        response = client.chat.completions.create(model=model_name, messages=chat_history)
         result = response.choices[0].message.content
-    elif model_name == "tokyotech-llm/Swallow-7b-instruct-hf":
-        chat_history = get_local_llm_prompt(chat_history)
-        result = get_local_llm_response(chat_history, model_name)
+    # elif model_name == "gpt-4-1106-preview":
+    #     chat_history = get_local_llm_prompt(chat_history)
+    #     result = get_local_llm_response(chat_history, model_name)
     else:
         raise
 
@@ -22,7 +21,7 @@ def llm_response(model_name, chat_history):
 def model_name_convert(model_name):
     if model_name == "gpt-3.5":
         return "gpt-3.5-turbo"
-    elif model_name == "Swallow":
-        return "tokyotech-llm/Swallow-7b-instruct-hf"
+    elif model_name == "gpt-4":
+        return "gpt-4-1106-preview"
     else:
         raise ValueError("Unknown model name")
